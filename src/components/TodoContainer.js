5eaa3f871119811035a0fb6221f7a3235b1d59ec
Camilovelag <camilo.vela.galindo@gmail.com>
2022-11-01 12:10:26 -0500

    Part 2 - Add text input field and submit btn

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,6 +1,8 @@
 import React from "react"
 import TodosList from "./TodosList"
 import Header from "./Header"
+import InputTodo from "./InputTodo"
+
 
 class TodoContainer extends React.Component {
   state = {
@@ -51,6 +53,7 @@ class TodoContainer extends React.Component {
     return (
       <div>
         <Header />
+        <InputTodo />
         <TodosList
           todos={this.state.todos}
           handleChangeProps={this.handleChange}
