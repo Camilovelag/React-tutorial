6f235cbba6defa237f1b12148665dc6ba70c152f
Camilovelag <camilo.vela.galindo@gmail.com>
2022-11-01 16:09:22 -0500

    Part 4 - Add Edit todos item description feature

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,32 +3,72 @@ import React from "react"
 import styles from "./TodoItem.module.scss"
 
 
-const TodoItem = (props) => {
-  const completedStyle = {
-    fontStyle: "italic",
-    color: "#595959",
-    opacity: 0.4,
-    textDecoration: "line-through",
+class TodoItem extends React.Component {
+  state = {
+    editing: false,
   }
 
-  const { completed, id, title } = props.todo;
+  handleEditing = () => {
+    this.setState({
+      editing: true,
+    })
+  }
 
-  return (
-    <li className={styles.item}>
-      <input
-        type="checkbox"
-        className={styles.checkbox}
-        checked={completed}
-        onChange={() => props.handleChangeProps(id)}
-      />
-      <button onClick={() => props.deleteTodoProps(id)}>
-        Delete
-      </button>
-      <span style={completed ? completedStyle : null}>
-        {title}
-      </span>
-    </li>
-  )
+  handleUpdatedDone = event => {
+    if (event.key === "Enter") {
+      this.setState({ editing: false })
+    }
+  }
+
+  render() {
+    const completedStyle = {
+      fontStyle: "italic",
+      color: "#595959",
+      opacity: 0.4,
+      textDecoration: "line-through",
+    }
+
+    const { completed, id, title } = this.props.todo;
+
+    let viewMode = {}
+    let editMode = {}
+
+    if (this.state.editing) {
+      viewMode.display = "none"
+    } else {
+      editMode.display = "none"
+    }
+
+    return (
+      <li className={styles.item}>
+        {/* change onclick by onDoubleClick */}
+        <div onClick={this.handleEditing} style={viewMode}>
+          <input
+            type="checkbox"
+            className={styles.checkbox}
+            checked={completed}
+            onChange={() => this.props.handleChangeProps(id)}
+          />
+          <button onClick={() => this.props.deleteTodoProps(id)}>
+            Delete
+          </button>
+          <span style={completed ? completedStyle : null}>
+            {title}
+          </span>
+        </div>
+        <input
+          type="text"
+          style={editMode}
+          className={styles.textInput}
+          value={title}
+          onChange={e => {
+            this.props.setUpdate(e.target.value, id)
+          }}
+          onKeyDown={this.handleUpdatedDone}
+        />
+      </li>
+    )
+  }
 }
 
 export default TodoItem
