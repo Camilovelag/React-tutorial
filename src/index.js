7c136630bf8b7c67f88a5d1d1e8a04a58a930d4b
Camilovelag <camilo.vela.galindo@gmail.com>
2022-11-01 12:39:27 -0500

    Part 3 - Add App.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import TodoContainer from './components/TodoContainer';
+//component file
+import TodoContainer from "./components/TodoContainer"
+
+//stylesheet
+import "./App.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
