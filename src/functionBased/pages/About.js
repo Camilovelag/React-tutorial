be6047dc5bac0e6b78b6b74a293ac2ee1a42f128
Camilovelag <camilo.vela.galindo@gmail.com>
2022-11-02 16:48:26 -0500

    Part 9 - Add Navigation and singlePage.js

--- src/functionBased/pages/About.js
+++ src/functionBased/pages/About.js
@@ -1,6 +1,13 @@
 import React from "react"
+import { Link, Outlet } from "react-router-dom"
 
 const About = () => {
-  return <div>hello from about page</div>
+  return (
+    <div>
+      <Link to='about-app'>About App</Link>
+      <Link to='about-author'>About Author</Link>
+      <Outlet />
+    </div>
+  )
 }
 export default About
--- src/index.js
+++ src/index.js
@@ -9,14 +9,19 @@ import NotMatch from "./functionBased/pages/NotMatch"
 
 //stylesheet
 import "./functionBased/App.css"
+import Navbar from './functionBased/components/Navbar';
+import SinglePage from './functionBased/pages/SinglePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<TodoContainer />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/about/*" element={<About />} >
+          <Route path=":slug" element={<SinglePage />} />
+        </Route>
         <Route path="*" element={<NotMatch />} />
       </Routes>
     </Router>
