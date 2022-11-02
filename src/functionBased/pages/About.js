bf8d670ea1bc5bb8838bd39fadb1a5e9074bf111
Camilovelag <camilo.vela.galindo@gmail.com>
2022-11-02 17:22:40 -0500

    Part 10 - Add toggle menu

--- src/functionBased/pages/About.js
+++ src/functionBased/pages/About.js
@@ -3,10 +3,12 @@ import { Link, Outlet } from "react-router-dom"
 
 const About = () => {
   return (
-    <div>
-      <Link to='about-app'>About App</Link>
-      <Link to='about-author'>About Author</Link>
-      <Outlet />
+    <div className="about__content">
+      <div className="about__list">
+        <Link to='about-app'>About App</Link>
+        <Link to='about-author'>About Author</Link>
+        <Outlet />
+      </div>
     </div>
   )
 }
