6044e64080430b2493365112f59fc43bace05fe9
Camilovelag <camilo.vela.galindo@gmail.com>
2022-11-02 19:32:56 -0500

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-//component file
-import TodoContainer from "./functionBased/components/TodoContainer"
-import About from "./functionBased/pages/About"
-import NotMatch from "./functionBased/pages/NotMatch"
+// component file
+import TodoContainer from './functionBased/components/TodoContainer';
+import About from './functionBased/pages/About';
+import NotMatch from './functionBased/pages/NotMatch';
 
-//stylesheet
-import "./functionBased/App.css"
+// stylesheet
+import './functionBased/App.css';
 import Navbar from './functionBased/components/Navbar';
 import SinglePage from './functionBased/pages/SinglePage';
 
@@ -19,11 +19,11 @@ root.render(
       <Navbar />
       <Routes>
         <Route path="/" element={<TodoContainer />} />
-        <Route path="/about/*" element={<About />} >
+        <Route path="/about/*" element={<About />}>
           <Route path=":slug" element={<SinglePage />} />
         </Route>
         <Route path="*" element={<NotMatch />} />
       </Routes>
     </Router>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
